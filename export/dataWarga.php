012ff280862fdea7288f10c4401a072c70a65091
bimaega15 <bimaega15@gmail.com>
2024-07-10 21:33:43 +0700

    Update Export Data Warga

--- export/dataWarga.php
+++ export/dataWarga.php
@@ -7,6 +7,46 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
 
+// Fetch and filter the data
+function explodeTanggal($data)
+{
+    $data = explode('/', $data);
+    return $data[2] . '-' . $data[1] . '-' . $data[0];
+}
+
+$dataGet = json_decode($_GET['filter'], true);
+$startDate = explodeTanggal($dataGet['tanggalPeriode']['startDate']);
+$endDate = explodeTanggal($dataGet['tanggalPeriode']['endDate']);
+$jenisKelamin = $dataGet['jenisKelamin'];
+$usia = $dataGet['usia'];
+
+$sql = "SELECT p.*, a.id_kk, k.no_kk, k.kepala, DATE_FORMAT(p.tgl_lh, '%d/%m/%Y') as tgl_lh 
+        FROM tb_pdd p 
+        LEFT JOIN tb_anggota a ON p.id_pend = a.id_pend 
+        LEFT JOIN tb_kk k ON a.id_kk = k.id_kk 
+        WHERE status = 'Ada'";
+if (!empty($startDate) && !empty($endDate)) {
+    $sql .= " AND DATE(p.updated_at) BETWEEN '$startDate' AND '$endDate'";
+}
+
+if (!empty($jenisKelamin)) {
+    $sql .= " AND p.jekel = '$jenisKelamin'";
+}
+
+$querySelect = mysqli_query($koneksi, $sql);
+$data = mysqli_fetch_all($querySelect, MYSQLI_ASSOC);
+$filterData = $data;
+
+if (!empty($usia)) {
+    $filterData = array_filter($data, function ($row) use ($usia) {
+        $birthdate = DateTime::createFromFormat('d/m/Y', $row['tgl_lh']);
+        $currentDate = new DateTime();
+        $age = $currentDate->diff($birthdate)->y;
+        return $age == $usia;
+    });
+}
+$data = $filterData;
+
 // Create a new Spreadsheet object
 $spreadsheet = new Spreadsheet();
 $activeWorksheet = $spreadsheet->getActiveSheet();
@@ -49,37 +89,6 @@ foreach ($header as $key => $heading) {
     $activeWorksheet->getColumnDimension($column)->setWidth($maxLen + 2);
 
     $column++;
-}
-
-// Fetch and filter the data
-$dataGet = json_decode($_GET['filter'], true);
-$startDate = date('Y-m-d', strtotime($dataGet['tanggalPeriode']['startDate']));
-$endDate = date('Y-m-d', strtotime($dataGet['tanggalPeriode']['endDate']));
-$jenisKelamin = $dataGet['jenisKelamin'];
-$usia = $dataGet['usia'];
-
-$sql = "SELECT p.*, a.id_kk, k.no_kk, k.kepala, DATE_FORMAT(p.tgl_lh, '%d/%m/%Y') as tgl_lh 
-        FROM tb_pdd p 
-        LEFT JOIN tb_anggota a ON p.id_pend = a.id_pend 
-        LEFT JOIN tb_kk k ON a.id_kk = k.id_kk 
-        WHERE status = 'Ada'";
-if (!empty($startDate) && !empty($endDate)) {
-    $sql .= " AND DATE(p.updated_at) BETWEEN '$startDate' AND '$endDate'";
-}
-if (!empty($jenisKelamin)) {
-    $sql .= " AND p.jekel = '$jenisKelamin'";
-}
-
-$querySelect = mysqli_query($koneksi, $sql);
-$data = mysqli_fetch_all($querySelect, MYSQLI_ASSOC);
-
-if (!empty($usia)) {
-    $data = array_filter($data, function ($row) use ($usia) {
-        $birthdate = DateTime::createFromFormat('d/m/Y', $row['tgl_lh']);
-        $currentDate = new DateTime();
-        $age = $currentDate->diff($birthdate)->y;
-        return $age == $usia;
-    });
 }
 
 // Populate the worksheet with data
